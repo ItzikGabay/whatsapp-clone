fa7b0e70c13037b78052f2a7638f1437b81e06df
ItzikDevio <itzikdevio@gmail.com>
2021-12-17 12:36:15 +0200

    Done with the navbar cloning

--- client/components/UI/navbar/Navbar.jsx
+++ client/components/UI/navbar/Navbar.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 
 export const Navbar = ({ ...props }) => {
-
-   return (
+  return (
     <div className="navbar-container">
       <div className="navbar-left">
         <div className="navbar-left-items">
-          <p>1</p>
-          <p className="navbar-item-name">2</p>
-          <p>3</p>
-          <p>4</p>
+          <img src="https://images.unsplash.com/photo-1549351512-c5e12b11e283?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="dev"/>
+          <p className="navbar-item-name">
+            Beta
+          </p>
+          <div>
+            <span class="material-icons-outlined">motion_photos_on</span>
+          </div>
+          <div>
+            <span class="material-icons">chat</span>
+          </div>
+          <div>
+            <span class="material-icons">more_vert</span>
+          </div>
         </div>
       </div>
-      <div className="navbar-right">2</div>
-  </div>
-   );
+      <div className="navbar-right">
+        <div className="navbar-right-items">
+          <img src="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="dev"/>
+          <p>Name of group</p>
+        </div>
+      </div>
+    </div>
+  );
 };
